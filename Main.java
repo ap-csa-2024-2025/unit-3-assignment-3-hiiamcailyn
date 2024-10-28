3dd4d56c2b849541a1fca74b1777087db0227c91
hiiamcailyn <cailyntran@gmail.com>
2024-10-28 09:53:52 -0700

    hi

--- Main.java
+++ Main.java
@@ -4,7 +4,19 @@ public class Main
 {
   public static void main(String[] args)
   {
+    public class boolean rectEquals(Rectngle r1, Rectangle rectEqual)
+    {
+      boolean equalWidth
+    }
     // TODO: Problem 1
+    int x = sc.nextInt();
+    int y = sc.nextInt(); 
+    System.out.println("Enter two numbers:" + x + y);
+    
+    if (y == 0)
+    {
+        System.out.println("Arith")
+    }
 
 
     // TODO: Problem 2
